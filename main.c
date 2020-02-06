ad32dccee4618229246d53b9d7d174dd3455d02f
Jason Licudan <danniel.licudan.563@my.csun.edu>
2020-02-05 18:08:13 -0800

    Updating

--- main.c
+++ main.c
@@ -1,89 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int main()
-{
+/* declaration */
+int square (int num);
+
+int main() {
+
+
+    int count = 0;
+    int num = 0;
+    int power = 6;
+
+    printf("Project Zero\n");
+
+    printf("Enter an 8-bit Binary Number!\n");
     char string[100];
 
-    int firstBit = 0;
-    int secondBit = 0;
-    int thirdBit = 0;
-    int fourthBit = 0;
-    int fifthBit = 0;
-    int sixthBit = 0;
-    int seventhBit = 0;
-    int eighthBit = 0;
-    int bitNum = 0;
-    int newNum = 0;
-    int anewNum = 0;
-    int power = 7;
+    gets(string);
 
-    printf("Enter Bits\n");
-    scanf("%s", string);
-    /*
-    if(string[0] == '1')
-    {
-        firstBit += 1;
-        //printf("%d", firstBit);
-        //printf("\nPrint this shit");
-    }
-    else if(string[1] == '1')
-    {
-        secondBit += 2;
-        printf("This is: %d", secondBit);
-    }
-    else if(string[2] == '1')
-    {
-        thirdBit += 4;
-    }
-    else if(string[3] == '1')
-    {
-        fourthBit += 8;
-    }
-    else if(string[4] == '1')
-    {
-         fifthBit += 16;
-    }
-    else if(string[5] == '1')
-    {
-        sixthBit += 32;
-    }
-    else if(string[6] == '1')
-    {
-        seventhBit += 64;
-    }
-    else if(string[7] == '1')
-    {
-        eighthBit += 128;
-    }*/
+    printf("String is: %s\n", string);
 
-    for(int i= 0; i < 8; i++)
+    while(string[count] != '\0')
     {
-        if(string[i] == '1')
+        if(string[0] == '1')
         {
-            anewNum = pow(2, power);
-            //newNum += pow(newNum, i);
-            printf("anewNum: %d\n", anewNum);
-            //printf("i: %d\n", i);*/
-            newNum += anewNum;
-            printf("newNum: %d\n", newNum);
-            //anewNum = 2;
-            power--;
-        }
-        else if(string[i] == '\0')
-        {
-            printf("Null\n");
+            printf("Too Much\n");
             break;
         }
 
+        if(string[count+1] == '1')
+        {
+
+            num += pow(2,power);
+
+        }
+
+        power--;
+        count++;
+
     }
 
-    bitNum = firstBit + secondBit + thirdBit + fourthBit + fifthBit + sixthBit + seventhBit + eighthBit;
+    printf("Number is: %d", num);
 
-    printf("%d", newNum);
-
-    printf("\nHello world!\n");
-    printf("Git Gud");
     return 0;
+}
+
+/* definition */
+int square (int num) {
+    int y;
+
+    y = num * num;
+
+    return(y);
 }
